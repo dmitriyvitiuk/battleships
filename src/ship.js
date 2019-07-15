f7a02cdb11fb88e5e633d8a258587c1963d82792
support <support@PTUA-LS0618.kie.pt.playtech.corp>
2019-07-15 13:14:20 +0300

    rr

--- src/ship.js
+++ src/ship.js
@@ -5,41 +5,39 @@ var step = 20;
 var currentX = 0;
 var currentY = 0;
 
+// function ShowDiv(){
+// document.getElementById("bomb_div").style.display = "block";
+// }
 
 document.onkeydown = function(event){
 //console.log(event)
-    if(event.key == "ArrowRight"){
+    if(event.key == "ArrowRight") {
         currentX = currentX + step;
         block.style.left = currentX +  "px"
     }
-    if(event.key == "ArrowDown"){
+    if(event.key == "ArrowDown") {
         currentY = currentY + step;
         block.style.top = currentY + "px"
     }
-    if(event.key == "ArrowLeft"){
+    if(event.key == "ArrowLeft") {
         currentX -= step;
-        block.style.left = currentX +  "px"
+        block.style.left = currentX + "px"
     }
-    if (event.key == "ArrowUp"){
+    if (event.key == "ArrowUp") {
         currentY -= step;
         block.style.top = currentY + 'px'
     }
-
-    
-     if (event.code == "Space") {
+    if (event.code == "Space") {
         console.log(event.key);
-
         bomb.className = 'animate-bomb';
-
-        createBomb(1,1)
-     }
+        createBomb(1,1);
+    }
       //console.log("["+event.key+"]" + " -> currentX = " + currentX +";  currentY = " + currentY)
     }
 
 
 
-    function createBomb( bombX, bombY)
-    {
+    function createBomb(bombX, bombY){
         var newDiv = document.createElement("div");
             newDiv.className = '';
             newDiv.innerHTML = ""
--- src/switch.js
+++
@@ -1,9 +0,0 @@
-function myFunction1(){
-
-    var x = document.getElementById("in_1").value;
-
-    console.log("convert to String = " + String(x));
-    console.log("convert to Number = " + Number(x));
-    console.log("convert to Boolean = " + Boolean(x));
-}
-
